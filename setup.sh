03294188cb8895ebcf9037966b9bcc4879bfa995
Ubuntu <ubuntu@ip-172-31-31-38.eu-central-1.compute.internal>
2015-12-01 21:18:42 +0000

    Added meteor install

--- setup.sh
+++ setup.sh
@@ -46,6 +46,7 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+curl https://install.meteor.com/ | sh
 #curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 #unzip awscli-bundle.zip
 #sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
